d27cf82402d6c96841b8323630253172f4120e85
CloneNOX <2511872384@qq.com>
2025-02-13 15:38:44 +0800

    25-2-13

---
+++ DDP_cp.sh
@@ -0,0 +1,30 @@
+set -v
+dataset="FB15k-237-subset-inductive" #FB15k-237-subset-inductive, NELL-995-subset-inductive, WN18RR-subset-inductive
+suffix="_full"
+finding_mode="head" #"head"
+device="cuda:0"
+seed=42
+epochs=30 #30
+relation_prediction_lr=1e-5
+
+path_search_depth=5
+path_support_type=2
+path_support_threshold=1e-4
+
+text_file='GoogleWikipedia'
+text_length=48
+
+min_search_depth=1
+max_search_depth=2
+
+rule_recall_threshold=0.5
+rule_accuracy_threshold=0.5
+
+# 10 DDP
+CUDA_VISIBLE_DEVICES="4,5,6,7" python -m torch.distributed.launch --nproc_per_node 4  DDP.py \
+ --device $device --epochs $epochs --batch_size 8 \
+ --dataset $dataset --learning_rate $relation_prediction_lr --neg_sample_num_train 5 \
+ --neg_sample_num_valid 50 --neg_sample_num_test 50 --mode $finding_mode --seed $seed \
+ --suffix $suffix --text_file $text_file --text_length $text_length \
+ --AP_load_setting cp\
+ --pos_triplets_path "data/data/$dataset/inductive_graph.txt" \
---
+++ DDP_ours1.sh
@@ -0,0 +1,31 @@
+set -v
+dataset="NELL-995-subset-inductive" #FB15k-237-subset-inductive, NELL-995-subset-inductive, WN18RR-subset-inductive
+suffix="_full"
+finding_mode="head" #"head"
+device="cuda:0"
+seed=42
+epochs=30 #30
+relation_prediction_lr=1e-5
+
+path_search_depth=5
+path_support_type=2
+path_support_threshold=1e-4
+
+text_file='GoogleWikipedia'
+text_length=48
+
+min_search_depth=1
+max_search_depth=2
+
+rule_recall_threshold=0.5
+rule_accuracy_threshold=0.5
+
+
+# 10 DDP
+CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345 DDP.py \
+ --device $device --epochs $epochs --batch_size 8 \
+ --dataset $dataset --learning_rate $relation_prediction_lr --neg_sample_num_train 5 \
+ --neg_sample_num_valid 50 --neg_sample_num_test 50 --mode $finding_mode --seed $seed \
+ --suffix $suffix --text_file $text_file --text_length $text_length \
+ --merge_max_path_num 3 --AP_load_setting ours\
+ --pos_triplets_path "data/data/$dataset/inductive_graph.txt"
